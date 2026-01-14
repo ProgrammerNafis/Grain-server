e1671d66f76dee65ad3cf6f7333f25c5f5abc999
ProgrammerNafis <sahariyanafis974@gmail.com>
2026-01-14 23:04:49 +0600

    update serializers.py

--- posts/serializers.py
+++ posts/serializers.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-from .models import Category, Post
+from .models import Post, Category, UserProfile, Comment 
 
 from django.contrib.auth.models import User
 from .models import UserProfile
